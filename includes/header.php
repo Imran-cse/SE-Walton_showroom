f8ea9a066c2df4a8abecfc8e15b5a1c84b2d28d7
Imran-cse <imran.cse.ku@gmail.com>
2017-08-18 10:39:31 +0600

    First commit

---
+++ includes/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../resources/style/css/carousel.css">
+    <link rel="stylesheet" type="text/css" href="../resources/style/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../resources/style/css/bootstrap-theme.css">
+    <link rel="stylesheet" href="../resources/style/fonts/glyphicons-halflings-regular.eot">
+    <link rel="stylesheet" href="../resources/style/fonts/glyphicons-halflings-regular.svg">
+
+    <script src="../resources/style/js/carousel.js"></script>
+    <title><?php echo ucfirst(basename($_SERVER['PHP_SELF'],'.php')) ?></title>
+</head>
+
+<nav class="navbar nav-tabs" style="background-color: none; border-bottom: inset;">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#" style="color: black;">Walton</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><a href="#"><span class="glyphicon glyphicon-home""></span>Home</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-info-sign"></span>About</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-earphone"></span>Contact</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        </ul>
+    </div>
+</nav>
---
+++ views/home.php
@@ -0,0 +1,29 @@
+<?php include "../includes/header.php"?>
+<body>
+
+<div class="c-carousel">
+    <div class="c-carousel__arrow c-carousel__arrow--left"></div>
+    <div class="c-carousel__arrow c-carousel__arrow--right"></div>
+    <ul class="c-carousel__slides">
+        <li><img src="../resources/images/imageedit_2_4647071428.jpg"/></li>
+        <li><img src="../resources/images/imageedit_4_5929328933.jpg" /></li>
+        <li><img src="../resources/images/imageedit_6_7091798663.jpg" /></li>
+        <li><img src="../resources/images/imageedit_8_2972809426.jpg" /></li>
+
+    </ul>
+</div>
+<div class="form-control">
+    <div style="padding-top: 50px; height: 2000px; ">
+        <p style="text-align: center; color: #2aabd2; font-size: 20px;">
+            Welcome to Walton Showroom of Khulna.
+        </p>
+    </div>
+    <div class="footer">
+        <p style="color: black">
+            copyright Walton Bangladesh Limited @2017
+        </p>
+    </div>
+</div>
+
+</body>
+</html>
