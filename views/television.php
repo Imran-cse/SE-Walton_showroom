3aaeaaa2dc636a2d67e9649cfe876cb260454721
Azoad Ahnaf <azoadahnaf@gmail.com>
2017-10-04 08:51:27 +0600

    TV gallery

--- views/television.php
+++ views/television.php
@@ -10,30 +10,31 @@
 					<a href="../resources/images/W43E3000AS.jpg" target="_blank">
 						<img src="../resources/images/W43E3000AS.jpg" alt="Walton W43E3000AS" style="width:256px;height:256px;">
 						<div class="caption" align="center">
-							<p>Walton Smart TV W43E3000AS</p>
+							<p>Walton Smart TV W43E3000AS(43")</p>
 						</div>
 					</a>
 				</div>
 			</div>
 			<div class="col-md-4">
 				<div class="thumbnail">
-					<a href="../resources/images/karonda.jpg" target="_blank">
-						<img src="../resources/images/karonda.jpg" alt="Walton Karonda" style="width:256px;height:256px;">
+					<a href="../resources/images/WD1-JX32-TS100.jpg" target="_blank">
+						<img src="../resources/images/WD1-JX32-TS100.jpg" alt="Walton WD1-JX32-TS100" style="width:256px;height:256px;">
 						<div class="caption" align="center">
-							<p>Karonda</p>
+							<p>Walton LED TV WD1-JX32-TS100(32")</p>
 						</div>
 					</a>
 				</div>
 			</div>
 			<div class="col-md-4">
 				<div class="thumbnail">
-					<a href="../resources/images/WT146U5S.jpeg" target="_blank">
-						<img src="../resources/images/WT146U5S.jpeg" alt="Walton Laptop WT146U5S" style="width:256px;height:256px;">
+					<a href="../resources/images/WT2178.jpg" target="_blank">
+						<img src="../resources/images/WT2178.jpg" alt="Walton WT2178" style="width:256px;height:256px;">
 						<div class="caption" align="center"> 
-							<p>Walton WT146U5S</p>
+							<p>Walton CRT TV WT2178</p>
 						</div>
 					</a>
 				</div>
 			</div>
 		</div>
+		
 </body>
