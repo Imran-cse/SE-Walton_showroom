a2f21012b0fd7151552126a861972225a0e9fa76
Azoad Ahnaf <azoadahnaf@gmail.com>
2017-10-15 00:10:05 +0600

    dummy wishlist & feedback

--- views/description.php
+++ views/description.php
@@ -25,7 +25,22 @@ include "../includes/header.php";
 					<h2><?php echo $value['name']; ?></h2>
 					<h2><?php echo $value['price']; ?><i class="icofont icofont-cur-taka"></i></h2>
 					<h4><strong>Quantity: </strong><?php echo $value['quantity']; ?></h4>
-					<p><strong>About: </strong><?php if ($value['description'] == null){echo 'Sorry! No description for this product.';} ?></p>
+					<p><strong>About: </strong><?php if ($value['description'] == null){echo 'Sorry! No description for this product.';} else {echo $value['description'];}?></p>
+					<p><a class="nounderline" onclick="show()" style="text-decoration: none !important;">Give Feedback about this product</a></p>
+					<div class="form-group hidden" id="feedback">
+						<label for="comment">Feedback:</label>
+						<textarea class="form-control" rows="5" id="comment" style="resize: none;"></textarea>
+					</div>
+					<script>
+						function show() {
+							var x = document.getElementById("feedback");
+							if (x.className == 'hidden') {
+								x.className = "unhidden";
+							} else {
+								x.className = "hidden";
+							}
+						}
+					</script>
 				</div>
 				<?php }
 		}
